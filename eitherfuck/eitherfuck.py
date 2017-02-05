12feb79961047bd9c1cf0a301ff52030b1d4ae13
saihv <saihemachandra.v@gmail.com>
2017-02-05 17:48:49 -0600

    Minor improvements

--- eitherfuck/eitherfuck.py
+++ eitherfuck/eitherfuck.py
@@ -14,21 +14,20 @@ class EF:
       self.ptr = 0 if self.ptr <= 0 else self.ptr - 1
 
     def add(self):
-      self.values[self.ptr] = self.values[self.ptr] + 1 if self.values[self.ptr] < 255 else 0
+      self.values[self.ptr] += 1 if self.values[self.ptr] < 255 else 0
 
     def subtract(self):
-      self.values[self.ptr] = self.values[self.ptr] - 1 if self.values[self.ptr] > 0 else 255
+      self.values[self.ptr] -= 1 if self.values[self.ptr] > 0 else 255
 
     def begin(self):
-       if self.values[self.ptr] == 0: self.loc = self.loopLoc[self.loc]
+       self.loc = self.loopLoc[self.loc] if self.values[self.ptr] == 0 else self.loc
 
     def end(self):
-       if self.values[self.ptr] != 0: self.loc = self.loopLoc[self.loc]
+       self.loc = self.loopLoc[self.loc] if self.values[self.ptr] != 0 else self.loc
 
     def read(self): self.values[self.ptr] = ord(getch.getch())
 
-    def write(self):
-        sys.stdout.write(chr(self.values[self.ptr]))
+    def write(self): sys.stdout.write(chr(self.values[self.ptr]))
 
     def opposite(self, cmd):
         if self.cmds.index(cmd)%2 == 0:
@@ -36,7 +35,6 @@ class EF:
         else:
             return self.cmds[self.cmds.index(cmd)-1]
         
-
     def BFify(self, EFcode):
         midPtr = int(len(EFcode)/2)
         ctr = 1
@@ -56,20 +54,19 @@ class EF:
         while self.loc < len(code):
             cmd = code[self.loc]
             if cmd in instructions:
-                instructions[cmd](self)      
+                instructions[cmd](self)
+
             self.loc += 1
 
     def minify(self, code):
         return list(filter(lambda x: x in ['.', ',', '[', ']', '<', '>', '+', '-', '0', '#'], code))
 
     def identifyLoops(self, code):
-        sbList = []
-        loopLoc = {}
+        sbList, loopLoc = [], {}
 
         for idx, cmd in enumerate(code):
             if cmd == '[': sbList.append(idx)
             if cmd == ']': start = sbList.pop();  loopLoc[start] = idx;  loopLoc[idx] = start
-
         return loopLoc
 
 instructions = {
@@ -85,7 +82,7 @@ instructions = {
 
 def main():
     Eitherfuck = EF()
-    f = open('examples\example.ef', 'r')
+    f = open('..\examples\example.ef', 'r')
     Eitherfuck.process(f.read())
     f.close()
 
